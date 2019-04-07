2b70d6d37a8bce75cfbaf23c649259a09759f584
Sudhanshu Raheja <sudhanshu@go-jek.com>
2019-04-08 01:27:41 +0700

    [sudhanshu] Added compression, basic logging

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const config = require('./config/config')
+const compression = require('compression')
+const morgan = require('morgan')
 
 const express = require('express')
 const app = express()
+
+app.use(morgan('combined'))
+app.use(compression())
+app.disable('x-powered-by')
+app.use((req, res, next) => {
+  res.header('X-Frame-Options', 'DENY')
+  res.header('X-Content-Type-Options', 'nosniff')
+  res.header('X-XSS-Protection', '1')
+  next()
+})
 
 app.get('/', (req, res) => res.json({ data: "hello world" }))
 app.use((req, res, next) => res.status(404).json({ error: "This endpoint does not exist" }))
