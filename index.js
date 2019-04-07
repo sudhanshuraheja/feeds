ff77d43ec78ef90c6493eafe04f5ddee198c141a
Sudhanshu Raheja <sudhanshu@go-jek.com>
2019-04-08 00:59:39 +0700

    [sudhanshu] Added express and config

---
+++ index.js
@@ -0,0 +1,8 @@
+const config = require('./config/config')
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
