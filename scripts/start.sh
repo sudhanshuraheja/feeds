a4d7134ea2afea568da38959b1f5fee5d5f6b823
Sudhanshu Raheja <sudhanshu@go-jek.com>
2019-04-07 23:08:04 +0700

    [sudhanshu] updated start script to enable management on rabbitmq

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Start Postgres
 docker run -p 5432:5432 --name feeds_pg -d postgres:10.7
+
+# Start Rabbit
 docker run -p 4369:4369 -p 5671:5671 -p 5672:5672 -p 15672:15672 --name feeds_rabbit -d rabbitmq:3.7.14
+
+# Start Redis
 docker run -p 6379:6379 --name feeds_redis -d redis:5.0.4
+
+# Start Rabbit Management console
+sleep 10
+docker exec feeds_rabbit rabbitmq-plugins enable rabbitmq_management
