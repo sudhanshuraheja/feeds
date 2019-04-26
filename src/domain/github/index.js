51511fc22841068549df3b29b35369a49c855633
Sudhanshu Raheja <sudhanshu@go-jek.com>
2019-04-26 13:19:06 +0800

    [sudhanshu] adds api call to github and tests

---
+++ src/domain/github/index.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+const logger = require('../../lib/logger')('domain/downloads')
+
+// Fetch data from this URL
+// https://api.github.com/repos/expressjs/express
+
+const github = {
+  url: (org, repo) => `https://api.github.com/repos/${org}/${repo}`,
+
+  api: async (org, repo) => {
+    try {
+      const result = await axios.get(github.url(org, repo))
+      return result.data
+    } catch (err) {
+      logger.error(err)
+      throw err
+    }
+  }
+
+}
+
+module.exports = github
