be46e7c7b14d0390fb498f6751cb22e6b3d6c066
Sudhanshu Raheja <sudhanshu@go-jek.com>
2019-04-29 00:26:51 +0700

    [sudhanshu] updates queries to make name and version unique throughout

--- migrations/001.do.npm.sql
+++ migrations/001.do.npm.sql
@@ -2,17 +2,15 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS sequence (
 	seq BIGINT PRIMARY KEY,
-	id VARCHAR(128),
+	name VARCHAR(128),
 	rev VARCHAR(64),
 	created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
 	updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS packages (
-	uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-	id VARCHAR(128),
+	name VARCHAR(128) PRIMARY KEY,
 	rev VARCHAR(128),
-	name VARCHAR(128),
 	description TEXT,
 	readme TEXT,
 	timeModified TIMESTAMP WITH TIME ZONE,
@@ -27,66 +25,15 @@ CREATE TABLE IF NOT EXISTS packages (
 	bugsEmail VARCHAR(128),
 	licenceType VARCHAR(64),
 	licenseURL VARCHAR(256),
+	users BIGINT,
 	created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
 	updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
-);
-
-CREATE TABLE IF NOT EXISTS distributionTags (
-	uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-	package UUID,
-	tag VARCHAR(64),
-	version VARCHAR(64),
-	created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-	updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
-);
-
-CREATE TABLE IF NOT EXISTS people (
-  uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  package UUID,
-	version UUID,
-	versionName VARCHAR(128),
-	email VARCHAR(64),
-	name VARCHAR(64),
-	url VARCHAR(64),
-  type VARCHAR(16), -- author / maintainers / contributors
-  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
-);
-
-CREATE TABLE IF NOT EXISTS versionTime (
-	uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  package UUID,
-	tag VARCHAR(64),
-	time TIMESTAMP WITH TIME ZONE,
-  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
-);
-
-CREATE TABLE IF NOT EXISTS npm (
-  uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  package UUID,
-  username VARCHAR(64),
-  liked BOOL,
-  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
-);
-
-CREATE TABLE IF NOT EXISTS keywords (
-  uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  package UUID,
-	version UUID,
-	versionName VARCHAR(128),
-  name VARCHAR(128),
-  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS versions (	
-  uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  package UUID,
-	id VARCHAR(128), -- npm@1.1.25
-  version VARCHAR(128),
-	name VARCHAR(128),
+	id VARCHAR(128) PRIMARY KEY, -- npm@1.1.25
+	name VARCHAR(128), -- npm
+  version VARCHAR(128), -- 1.1.25
 	description VARCHAR(256),
 	homepage VARCHAR(256),
 	repositoryType VARCHAR(16),
@@ -108,13 +55,60 @@ CREATE TABLE IF NOT EXISTS versions (
   updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
 
+CREATE UNIQUE INDEX IF NOT EXISTS versions_name_version_idx ON versions(name, version);
+
+CREATE TABLE IF NOT EXISTS tags ( -- distribution-tags
+	uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	name VARCHAR(128),
+	tag VARCHAR(64),
+	version VARCHAR(64),
+	created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+	updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS tags_name_tag_idx ON tags (name, tag);
+
+CREATE TABLE IF NOT EXISTS people (
+  uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  name VARCHAR(128),
+	version VARCHAR(128),
+	email VARCHAR(64),
+	name VARCHAR(64),
+	url VARCHAR(64),
+  type VARCHAR(16), -- author / maintainers / contributors
+  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS people_name_version_idx ON people(name, version);
+
+CREATE TABLE IF NOT EXISTS times (
+	uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  name VARCHAR(128),
+	version VARCHAR(64),
+	time TIMESTAMP WITH TIME ZONE,
+  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS times_name_version_idx ON times(name, version);
+
+CREATE TABLE IF NOT EXISTS keywords (
+  uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  name VARCHAR(128),
+	version VARCHAR(64),
+  keyword VARCHAR(128),
+  created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS keywords_name_version_keyword_idx ON keywords(name, version, keyword);
+
 CREATE TABLE IF NOT EXISTS dependencies (
   uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  package UUID,
-	version UUID,
-	versionName VARCHAR(128),
   name VARCHAR(128),
-	mappedPackage UUID,
+	version VARCHAR(64),
+  dependency VARCHAR(128),
 	semver VARCHAR(64),
 	url VARCHAR(128),
   type VARCHAR(8), -- dep / bundle / dev / optional
@@ -122,3 +116,4 @@ CREATE TABLE IF NOT EXISTS dependencies (
   updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
 
+CREATE UNIQUE INDEX IF NOT EXISTS dependencies_name_version_dependenct_idx ON keywords(name, version, dependency);
