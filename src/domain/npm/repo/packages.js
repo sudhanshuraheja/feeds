ad1b780a3f1720820d892b749394359e797f8fe8
Sudhanshu Raheja <sudhanshu@go-jek.com>
2019-04-29 18:42:39 +0700

    [sudhanshu] adds repository for packages

---
+++ src/domain/npm/repo/packages.js
@@ -0,0 +1,33 @@
+const db = require('../../../lib/db')
+
+const packages = {
+  get: async (name) => {
+    if (typeof name !== 'string') throw new Error('[packages] name should be a string')
+    if (name.length > 128) throw new Error('[packages] name should be less than 128 chars')
+
+    try {
+      const result = await db.query(`SELECT * FROM packages WHERE name=$1`, [name])
+      return result
+    } catch (err) {
+      throw err
+    }
+  },
+
+  insert: async (name, rev, description, readme, timeModified, timeCreated, repositoryType, repositoryURL, repositoryGithubOrg, repositoryGithubRepo, readmeFileName, homepage, bugsURL, bugsEmail, licenceType, licenseURL, users) => {
+    // if (!Number.isInteger(seq)) throw new Error('[packages] seq should be an integer')
+    // if (seq <= 0) throw new Error('[packages] seq should be greater than 0')
+    if (typeof name !== 'string') throw new Error('[packages] name should be a string')
+    if (name.length > 128) throw new Error('[packages] name should be less than 128 chars')
+    if (typeof rev !== 'string') throw new Error('[packages] rev should be a string')
+    if (rev.length > 128) throw new Error('[packages] rev should be less than 128 chars')
+
+    try {
+      const result = await db.query(`INSERT INTO packages(name, rev, description, readme, timeModified, timeCreated, repositoryType, repositoryURL, repositoryGithubOrg, repositoryGithubRepo, readmeFileName, homepage, bugsURL, bugsEmail, licenceType, licenseURL, users) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) RETURNING *`, [name, rev, description, readme, timeModified, timeCreated, repositoryType, repositoryURL, repositoryGithubOrg, repositoryGithubRepo, readmeFileName, homepage, bugsURL, bugsEmail, licenceType, licenseURL, users])
+      return result
+    } catch(err) {
+      throw err
+    }
+  }
+}
+
+module.exports = packages
---
+++ src/domain/npm/repo/packages.test.js
@@ -0,0 +1,35 @@
+const moment = require('moment')
+const packages = require('./packages')
+const db = require('../../../lib/db')
+const config = require('../../../lib/config')
+
+describe('Repo Packages', () => {
+  beforeAll(async () => {
+    config.init()
+    await db.connect()
+    await db.truncate('packages')
+  })
+
+  afterAll(async () => {
+    await db.end()
+  })
+
+  test('Check get', async () => {
+    const result = await packages.get('node-dummy')
+    expect(result.rows).toEqual([])
+  })
+
+  test('Check insert', async () => {
+    const now = moment().format()
+    try {
+      await packages.insert(null, 'pkg-rev', 'long description', '#readme', now, now, 'git', 'git://url', 'org', 'repo', 'readme.md', 'https://homepage.org', 'http://bugs.org', 'bugs@org.com', 'MIT', 'http://license.org', 15)
+    } catch (err) {
+      expect(err.toString()).toMatch(/[sequence]/)
+    }
+
+    const updated = moment('2013-03-16T18:45:36.782Z').format()
+    await packages.insert('pkg-name', 'pkg-rev', 'long description', '#readme', now, updated, 'git', 'git://url', 'org', 'repo', 'readme.md', 'https://homepage.org', 'http://bugs.org', 'bugs@org.com', 'MIT', 'http://license.org', 15)
+    const result = await packages.get('pkg-name')
+    expect(result.rows[0].name).toBe('pkg-name')
+  })
+})
